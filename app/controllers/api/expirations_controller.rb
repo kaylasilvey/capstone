d43903d7821028bb6a73bfba8adbc625dac3218f
Kayla Silvey <ksilvey19@gmail.com>
2019-07-31 20:17:03 -0500

    defines RESTful route models for Expirations, builds index/show/partial files

--- app/controllers/api/expirations_controller.rb
+++ app/controllers/api/expirations_controller.rb
@@ -1,16 +1,36 @@
 class Api::ExpirationsController < ApplicationController
   def index
+    @expirations = Expiration.all
+    render "index.json.jb"
   end
 
   def show
+    @expiration = Expiration.find_by(id: params[:id])
+    render "show.json.jb"
   end
 
   def create
+    @expiration = Expiration.new(
+      date: params[:date],
+      user_id: current_user.id,
+    )
+    if @expiration.save
+      render "show.json.jb"
+    else
+      render json: { errors: @expiration.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
+    @expiration = Expiration.find_by(id: params[:id])
+    @expiration.date = params[:date] || @expiration.date
+    @expiration.save
+    render "show.json.jb"
   end
 
   def destroy
+    @expiration = Expiration.find_by(id: params[:id])
+    @expiration.destroy
+    render json: { message: "This Expiration has been deleted." }
   end
 end
